261f1c61c7b7d47536906b64ee5bc43cef0b2a08
DESKTOP-T324LQ6\CESAR-PC <rivera.csar@gmail.com>
2018-11-05 21:45:55 -0600

    Se agregan archivos necesarios para generar árbol, falta depurarlo y generar tabla de símbolos

---
+++ STC.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class STC extends Object {
+
+
+  String type;  // type,in calculator expample there only is one type==> Int.
+  String value; // value, the value in here is a string not a integer value.
+
+  public STC(String itype, String ivalue) {
+    type=itype;
+    value=ivalue;
+
+  }
+}
