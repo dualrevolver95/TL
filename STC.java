1f772b2cbb49084a00f5365b3d4df0c4ccfc38a6
Rafael Alejo <rafael_alejo@outlook.com>
2018-11-08 22:36:33 -0600

    Agregado manejo de ambito

--- STC.java
+++ STC.java
@@ -3,14 +3,27 @@ import java.util.*;
 public class STC extends Object {
 
 
-  String type;  // type,in calculator expample there only is one type==> Int.
-  String value; // value, the value in here is a string not a integer value.
-  int numLineas;
+    public  String type;  // type,in calculator expample there only is one type==> Int.
+    public  String value; // value, the value in here is a string not a integer value.
+    public  int numLineas;
+    public  SimpleNode ambito;
 
-  public STC(String itype, String ivalue, int iNumLineas) {
-    type=itype;
-    value=ivalue;
-    numLineas=iNumLineas;
+    public STC(String itype, String ivalue, int iNumLineas) {
+        type = itype;
+        value = ivalue;
+        numLineas = iNumLineas;
 
-  }
+    }
+
+    public STC(String itype, String ivalue, int iNumLineas, Object iambito) {
+        type = itype;
+        value = ivalue;
+        numLineas = iNumLineas;
+        ambito = (SimpleNode) iambito;
+    }
+
+    public String getAmbitoString() {
+        return ambito.toString();
+    }
+
 }
